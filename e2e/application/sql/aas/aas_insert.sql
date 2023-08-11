e21169b942c43864f2098dc73fd799e8e9dca147
hiroaki.yoshii <orient110yh@gmail.com>
2023-08-11 19:51:00 +0900

    fix test

--- e2e/application/sql/aas/aas_insert.sql
+++ e2e/application/sql/aas/aas_insert.sql
@@ -3,10 +3,14 @@ INSERT INTO aas VALUES ('smart.festo.com/demo/aas/1/1/454576463545648365874', '{
 
 INSERT INTO submodel_proxy VALUES 
 ('www.company.com/ids/sm/4343_5072_7091_3242', 'Nameplate', 'https://www.hsu-hh.de/aut/aas/nameplate', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp),
--- ('smart.festo.com\demo\sm\instance\1\1\13B7CCD9BF7A3F24','DeviceDescriptionFiles', 'http://admin-shell/sample/submodel/type/device-description-files', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp),
--- ('www.company.com/ids/sm/6053_5072_7091_5102','Service', 'https://www.hsu-hh.de/aut/aas/service', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp),
-('www.company.com/ids/sm/6563_5072_7091_4267','Identification', 'https://www.hsu-hh.de/aut/aas/identification', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp),
--- ('www.company.com/ids/sm/2543_5072_7091_2660','Document', 'https://www.hsu-hh.de/aut/aas/document', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp)
+('www.company.com/ids/sm/6563_5072_7091_4267','Identification', 'https://www.hsu-hh.de/aut/aas/identification', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp)
 ;
 
+-- INSERT INTO submodel_proxy VALUES 
+-- ('www.company.com/ids/sm/4343_5072_7091_3242', 'Nameplate', 'https://www.hsu-hh.de/aut/aas/nameplate', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp),
+-- ('smart.festo.com\demo\sm\instance\1\1\13B7CCD9BF7A3F24','DeviceDescriptionFiles', 'http://admin-shell/sample/submodel/type/device-description-files', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp),
+-- ('www.company.com/ids/sm/6053_5072_7091_5102','Service', 'https://www.hsu-hh.de/aut/aas/service', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp),
+-- ('www.company.com/ids/sm/6563_5072_7091_4267','Identification', 'https://www.hsu-hh.de/aut/aas/identification', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp),
+-- ('www.company.com/ids/sm/2543_5072_7091_2660','Document', 'https://www.hsu-hh.de/aut/aas/document', 'smart.festo.com/demo/aas/1/1/454576463545648365874', current_timestamp,current_timestamp)
+-- ;
 
