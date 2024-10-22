591f1d3920b1b124f5077028b1f63ea3a8a2afb9
hiroyoshii <orient110yh@gmail.com>
2024-10-22 23:52:41 +0900

    fix ci

--- e2e/scenario_setup.sh
+++ e2e/scenario_setup.sh
@@ -1,13 +1,13 @@
-docker compose -f e2e/docker-compose.yaml up -d --wait
+docker compose -f e2e/docker compose.yaml up -d --wait
 
-docker compose -f e2e/docker-compose.yaml exec -T postgres_aas psql -U postgres aas -f /app/aas_schema.sql
-docker compose -f e2e/docker-compose.yaml exec -T postgres_aas psql -U postgres aas -f /app/sql/aas_insert.sql
+docker compose -f e2e/docker compose.yaml exec -T postgres_aas psql -U postgres aas -f /app/aas_schema.sql
+docker compose -f e2e/docker compose.yaml exec -T postgres_aas psql -U postgres aas -f /app/sql/aas_insert.sql
 
-docker compose -f e2e/docker-compose.yaml exec -T postgres_submodel1 psql -U postgres submodel1 -f /app/sql/submodel1_ddl.sql
-docker compose -f e2e/docker-compose.yaml exec -T postgres_submodel1 psql -U postgres submodel1 -f /app/sql/submodel1_insert.sql
+docker compose -f e2e/docker compose.yaml exec -T postgres_submodel1 psql -U postgres submodel1 -f /app/sql/submodel1_ddl.sql
+docker compose -f e2e/docker compose.yaml exec -T postgres_submodel1 psql -U postgres submodel1 -f /app/sql/submodel1_insert.sql
 
-docker compose -f e2e/docker-compose.yaml exec -T postgres_submodel2 psql -U postgres submodel2 -f /app/sql/submodel2_ddl.sql
-docker compose -f e2e/docker-compose.yaml exec -T postgres_submodel2 psql -U postgres submodel2 -f /app/sql/submodel2_insert.sql
+docker compose -f e2e/docker compose.yaml exec -T postgres_submodel2 psql -U postgres submodel2 -f /app/sql/submodel2_ddl.sql
+docker compose -f e2e/docker compose.yaml exec -T postgres_submodel2 psql -U postgres submodel2 -f /app/sql/submodel2_insert.sql
 
-docker compose -f e2e/docker-compose.yaml logs
-docker compose -f e2e/docker-compose.yaml ps
+docker compose -f e2e/docker compose.yaml logs
+docker compose -f e2e/docker compose.yaml ps
